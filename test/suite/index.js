1167b64b2ee0dc529c0adc639bc822097d54f185
YashTotale <totaleyash@gmail.com>
2020-07-31 16:04:04 -0700

    further simplified test/suite/index.js

--- test/suite/index.js
+++ test/suite/index.js
@@ -2,49 +2,55 @@ const path = require("path");
 const Mocha = require("mocha");
 const glob = require("glob");
 
-function createMochaInstance() {
+function createMocha() {
   return new Mocha({
     ui: "tdd",
     color: true,
   });
 }
 
-function createGlob(testsRoot, func) {
-  glob("**/**.test,js", { cwd: testsRoot }, (err, files) => func(err, files));
+function addFiles(files, mocha, testsRoot) {
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 }
 
 function runMocha(mocha, c, e) {
   mocha.run((failures) => {
     if (failures > 0) {
-      return e(new Error(`${failures} tests failed.`));
+      e(new Error(`${failures} tests failed.`));
+      return undefined;
     }
-    return c();
+    c();
+    return undefined;
   });
 }
 
-function run() {
+function globFunc(err, files, testsRoot, c, e) {
   // Create the mocha test
-  const mocha = createMochaInstance();
+  const mocha = createMocha();
 
+  if (err) {
+    return e(err);
+  }
+
+  // Add files to the test suite
+  addFiles(files, mocha, testsRoot);
+
+  try {
+    // Run the mocha test
+    return runMocha(mocha, c, e);
+  } catch (err) {
+    console.error(err);
+    return e(err);
+  }
+}
+
+function run() {
   const testsRoot = path.resolve(__dirname, "..");
 
   return new Promise((c, e) => {
-    createGlob(testsRoot, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        return runMocha(mocha, c, e);
-      } catch (err) {
-        console.error(err);
-        return e(err);
-      }
-    });
+    glob("**/**.test.js", { cwd: testsRoot }, (err, files) =>
+      globFunc(err, files, testsRoot, c, e)
+    );
   });
 }
 
