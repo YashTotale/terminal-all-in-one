422f99a8567a18642a68038a930f3d4777bf57b8
YashTotale <totaleyash@gmail.com>
2020-08-01 21:56:48 -0700

    modified tests

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,26 +1,15 @@
+// @ts-nocheck
 const assert = require("assert");
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require("vscode");
 // @ts-ignore
-const themes = require("../../src/themes.json");
+const { activate, deactivate } = require("../../src/extension.js");
 // const myExtension = require('../extension');
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
-
-  test("Themes", () => {
-    // @ts-ignore
-    assert.ok(themes, "Themes object exists");
-    // @ts-ignore
-    assert.ok(themes.length, "Themes object has objects");
-
-    // for (const themeName in themes) {
-    //   const themeKeys = Object.keys(themes[themeName]);
-
-    // }
-  });
 
   return undefined;
 });
---
+++ test/suite/themes.test.js
@@ -0,0 +1,43 @@
+// @ts-nocheck
+const assert = require("assert");
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+const vscode = require("vscode");
+// @ts-ignore
+const themes = require("../../src/themes.json");
+// const myExtension = require('../extension');
+
+suite("Themes Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
+
+  test("Themes object exists", () => {
+    assert.ok(themes, "Themes object exists");
+  });
+
+  test("Themes object has keys", () => {
+    assert.ok(themes.length, "Themes object has objects");
+  });
+
+  test("Theme has correct keys", () => {
+    for (const themeName in themes) {
+      const themeKeys = Object.keys(themes[themeName]);
+      const keys = ["name", "colors"];
+
+      assert.deepStrictEqual(
+        keys.length,
+        themeKeys.length,
+        "Theme object has correct number of keys"
+      );
+
+      keys.forEach((key) => {
+        assert.ok(
+          themeKeys.some((themeKey) => themeKey === key),
+          "Theme object has correct key"
+        );
+      });
+    }
+  });
+
+  return undefined;
+});
