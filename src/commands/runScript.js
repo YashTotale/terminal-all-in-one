fda5394dc094294c5ece81d0135a2a4a40e556d9
YashTotale <totaleyash@gmail.com>
2020-08-07 12:27:35 -0700

    array scripts working; script message working

--- src/commands/runScript.js
+++ src/commands/runScript.js
@@ -6,14 +6,53 @@ const getScriptsConfig = function () {
   return getConfig({ section: "terminalAllInOne.scripts" });
 };
 
-const execute = async function ({ script }) {
-  await vscode.commands.executeCommand("workbench.action.terminal.focus");
+const runInTerminal = async function (command) {
   return vscode.commands.executeCommand(
     "workbench.action.terminal.sendSequence",
     {
-      text: `\u0003 ${script} \u000D`,
+      text: command,
     }
   );
+};
+
+const createRunningCommand = function ({ name, script }) {
+  function echoCmd(cmd, num) {
+    return `echo -e "\\t ${num}. ${cmd}"`;
+  }
+  const controlC = "\u0003";
+  const emptyLine = 'echo "";';
+  const running = `echo -e "Running script: \\033[1m${name}\\033[0m";`;
+  const enter = "\u000D";
+  let cmds = "";
+  if (typeof script === "string") {
+    cmds = echoCmd(script, 1);
+  } else {
+    script.forEach((s, i) => {
+      cmds += echoCmd(s, i + 1);
+      if (i !== script.length - 1) {
+        cmds += " ; ";
+      }
+    });
+  }
+  return `${controlC} ${emptyLine} ${running} ${emptyLine} ${cmds} ${enter}`;
+};
+
+const createCommands = function (script) {
+  if (typeof script === "string") {
+    return script;
+  }
+  let jointScript = "";
+  script.forEach((s, i) => {
+    jointScript += i ? ` && ${s}` : s;
+  });
+  return jointScript;
+};
+
+const execute = async function ({ name, script }) {
+  const cmds = createCommands(script);
+  await vscode.commands.executeCommand("workbench.action.terminal.focus");
+  await runInTerminal(createRunningCommand({ name, script }));
+  await runInTerminal(`${cmds} \u000D`);
 };
 
 const runScript = async function (index) {
@@ -29,7 +68,7 @@ const runScript = async function (index) {
   }
   const options = scripts.map(({ name, script }, i) => ({
     label: name,
-    description: script,
+    description: Array.isArray(script) ? script.join(" -> ") : script,
     index: i,
   }));
   const selectedScript = await vscode.window.showQuickPick(options, {
