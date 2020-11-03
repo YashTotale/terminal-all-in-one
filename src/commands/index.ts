1b183c6dd2238d9fa3f406c5359dd8221c97dddd
YashTotale <totaleyash@gmail.com>
2020-11-03 12:15:47 -0800

    Create new terminal cmd added

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,12 +3,14 @@ import ClearTerminal from "./clearTerminal";
 import RunScript from "./runScript";
 import SelectDefaultShell from "./selectDefaultShell";
 import ToggleMaxTerm from "./toggleMaxTerm";
+import CreateNewTerminal from "./createNewTerminal";
 import ChangeFontSize from "./fontSize/changeFontSize";
 import AdjustFontSizeByOne from "./fontSize/adjustFontSizeByOne";
 import { ExtensionContext } from "vscode";
 
 export default (context: ExtensionContext) => [
   new SelectDefaultShell(context),
+  new CreateNewTerminal(context),
   new ChooseTerminalTheme(context),
   new ClearTerminal(context),
   new RunScript(context),
